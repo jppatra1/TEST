80c657b350d75730bfb449bf30983804f9ce1430
jppatra1 <36302898+jppatra1@users.noreply.github.com>
2018-11-17 22:41:38 +0530

    changed a.txt to server.log

--- active.sh
+++ active.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sv_name='192.168.0.105'
 counter=0
-echo " Started a new run " > a.txt
+echo " Started a new run " > server.log
 while [ True ]
 do
 ping -c 3 $sv_name >/dev/null 2>&1
 if [ $? -eq 0 ]
 then
-echo " Server is active , This is Counter $counter " >> a.txt
+echo " Server is active , This is Counter $counter " >> server.log
 sleep 10 ;
 counter=`expr $counter + 1`
 if [ $counter -eq 5 ]
@@ -17,7 +17,7 @@ else
 continue ;
 fi
 else
-echo " $sv_name is not active " >> a.txt
+echo " $sv_name is not active " >> server.log
 exit 1 ;
 fi
 done
